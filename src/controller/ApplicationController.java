2f310ff6155c3cd5a64a26346c537ed503aa0f22
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 12:59:07 +0100

    partly implemented method findAllBooks

--- src/controller/ApplicationController.java
+++ src/controller/ApplicationController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import dao.LibraryDB;
+import dao.LibraryDBImpl;
+import model.Library;
 import view.ApplicationView;
 import dao.ConnectionManager;
 
@@ -8,19 +11,21 @@ public class ApplicationController {
     private final String HEADER;
     private final String[] OPTIONS;
     private ApplicationView view;
+    private LibraryDB libraryDB;
 
     public ApplicationController() {
         this.HEADER = "Library Browser 1.0";
         this.OPTIONS = new String[]{"Add book to library", "Edit book",
                 "Delete book from library", "Search for book", "Display all books", "Display book by author"};
         this.view = new ApplicationView();
+        this.libraryDB = new LibraryDBImpl();
     }
 
     public void run() {
         String option = "1";
 
         while (!(option.equals("4"))) {
-
+            Library.library = libraryDB.findAllBooks();
             view.displayMenu(HEADER, OPTIONS);
             option = InputController.getString("Choose option: ");
 
@@ -34,6 +39,7 @@ public class ApplicationController {
                 case "3":
                     break;
                 case "4":
+
                     break;
                 case "5":
                     break;
--- src/dao/LibraryDBImpl.java
+++ src/dao/LibraryDBImpl.java
@@ -1,4 +1,62 @@
 package dao;
 
+import model.Author;
+import model.Book;
+import model.Publisher;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryDBImpl implements LibraryDB {
+
+
+    @Override
+    public List<Book> findAllBooks() {
+        Connection connection = ConnectionManager.getNewConnection();
+        String url = "SELECT type_book.type FROM books " +
+                "INNER JOIN authors ON books.author=authors.author_id " +
+                "INNER JOIN  publishers ON books.publisher=publishers.publisher_id " +
+                "INNER JOIN type_book ON books.type=type_book.type_id " +
+                "WHERE books.author=authors.author_id AND " +
+                "books.publisher=publishers.publisher_id;";
+        Statement newStatement = null;
+        ResultSet resultSet = null;
+        List<Book> books = new ArrayList<>();
+        Book newBook = null;
+        Author author = null;
+        Publisher publisher = null;
+        String title;
+        String type;
+        int publicationYear;
+        int isbn;
+        double price;
+
+        try {
+            newStatement = connection.createStatement();
+            resultSet = newStatement.executeQuery(url);
+            while (resultSet.next()) {
+                int authorId = resultSet.getInt("author_id");
+                String authorCity = resultSet.getString("city");
+                String authorCountry = resultSet.getString("country");
+                String authorName = resultSet.getString("name");
+                String authorSurname = resultSet.getString("surname");
+                int birthdate = resultSet.getInt("birthday");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        return books;
+    }
+
+    @Override
+    public void updateBooks() {
+
+    }
+
+
 }
