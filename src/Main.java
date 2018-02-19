cdf52af5e604dde5e551d85f02158c55f8585fe1
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 11:38:35 +0100

    add main method to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import controller.ApplicationController;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ApplicationController controller =  new ApplicationController();
+        controller.run();
     }
 }
