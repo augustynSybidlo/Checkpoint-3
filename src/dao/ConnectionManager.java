5ece08dac7b7abd965b38c4a5d0777d1e4d911c2
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 11:18:07 +0100

    add new class ConnnectionManager with implementation

--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -1,4 +1,37 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+
+
+
+    public static Connection newConnection = establishConnection();
+
+    private static Connection establishConnection() {
+        Connection newConnection = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            newConnection = DriverManager.getConnection(
+                    "jdbc:sqlite:/home/augustyn/Documents/MyCode/Java files/Checkpoint-3/src/library.db");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(00);
+        }
+        return newConnection;
+    }
+
+    public static void closeConnection(Connection connectionToClose) {
+        try {
+            connectionToClose.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(11);
+        }
+    }
+    public static Connection getNewConnection() {
+        return newConnection;
+    }
 }
