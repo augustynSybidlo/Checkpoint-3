d9412ad0f1362e3d82bdd794b4122eb4bbd6cc2b
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 10:26:29 +0100

    add abstract class Person with implementation to model package

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,5 +2,37 @@ package model;
 
 public abstract class Person {
 
+    private int id;
+    private String city;
+    private String country;
 
+    public Person(int id, String city, String country) {
+        this.id = id;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
