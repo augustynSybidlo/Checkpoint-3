514400eff997101019c235a758e9b101165af260
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 10:29:40 +0100

    implement Publisher model

--- src/model/Publisher.java
+++ src/model/Publisher.java
@@ -1,6 +1,19 @@
 package model;
 
-public class Publisher {
+public class Publisher extends Person {
 
+    private String name;
 
+    public Publisher(int id, String city, String country, String name) {
+        super(id, city, country);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
