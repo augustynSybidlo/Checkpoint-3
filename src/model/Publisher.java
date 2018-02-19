d578c727c00b7d0292f9a866d04d104cd057b52b
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 10:36:00 +0100

    add toString method in Publisher class

--- src/model/Publisher.java
+++ src/model/Publisher.java
@@ -16,4 +16,10 @@ public class Publisher extends Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Publisher name: %s \n city: %s \n country: %s \n id: %d",
+                name, getCity(), getCountry(), getId());
+    }
 }
