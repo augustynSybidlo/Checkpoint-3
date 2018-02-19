10507ca622aab63dea69eac19f1f9ba324e0dc87
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 11:39:23 +0100

    add run method to ApplicationController class

--- src/controller/ApplicationController.java
+++ src/controller/ApplicationController.java
@@ -1,6 +1,46 @@
 package controller;
 
+import view.ApplicationView;
+import dao.ConnectionManager;
+
 public class ApplicationController {
 
+    private final String HEADER;
+    private final String[] OPTIONS;
+    private ApplicationView view;
 
+    public ApplicationController() {
+        this.HEADER = "Library Browser 1.0";
+        this.OPTIONS = new String[]{"Add book to library", "Edit book",
+                "Delete book from library", "Search for book", "Display all books", "Display book by author"};
+        this.view = new ApplicationView();
+    }
+
+    public void run() {
+        String option = "1";
+
+        while (!(option.equals("4"))) {
+
+            view.displayMenu(HEADER, OPTIONS);
+            option = InputController.getString("Choose option: ");
+
+            switch (option) {
+                case "0":
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    ConnectionManager.closeConnection(ConnectionManager.getNewConnection());
+                    view.displayText("Good bye!");
+            }
+        }
+    }
 }
