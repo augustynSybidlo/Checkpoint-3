2d8657faa8afadb5573123c36c0ce9b63760f7e6
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 11:01:21 +0100

    add Library class to model package with implementation

--- src/model/Library.java
+++ src/model/Library.java
@@ -1,4 +1,16 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Library {
+
+    private static ArrayList<Book> library = new ArrayList<>();
+
+    public static ArrayList<Book> getLibrary() {
+        return library;
+    }
+
+    public static void setLibrary(ArrayList<Book> library) {
+        Library.library = library;
+    }
 }
