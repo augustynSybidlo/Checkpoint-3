df387c8a441d32e5822b7b291f64729451969e30
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-02-19 11:18:46 +0100

    add new class ApplicationView with implementation

--- src/view/ApplicationView.java
+++ src/view/ApplicationView.java
@@ -1,4 +1,29 @@
 package view;
 
-public class AbstractView {
+import java.util.ArrayList;
+
+public class ApplicationView<T> {
+
+    public void displayText(String text) {
+        System.out.println(text);
+    }
+
+    public void displayListOfObjects(ArrayList<T> listOfObjects) {
+        int index = 0;
+        for (T object : listOfObjects) {
+            displayText(index + "-->" + object.toString());
+            index++;
+        }
+    }
+
+    public void displayMenu(String header, String[] options) {
+
+        displayText(header);
+        int index = 0;
+
+        for (String option : options) {
+            displayText(index + "-->" + option);
+            index++;
+        }
+    }
 }
